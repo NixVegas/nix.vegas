483baa20edd7ce11180ec12865ed417e461c1c3b
Morgan Jones <me@numin.it>
2025-08-05 04:58:12 +0000

    onboarding: update with artifacts

--- pkgs/nix-vegas-site/onsite.nix
+++ pkgs/nix-vegas-site/onsite.nix
@@ -2,8 +2,8 @@
   nix-vegas-site,
   jq,
   yq,
-  emptyDir,
-  onboardingArtifacts ? emptyDir,
+  emptyDirectory,
+  onboardingArtifacts ? emptyDirectory,
   ...
 }:
 
@@ -15,26 +15,26 @@ nix-vegas-site.overrideAttrs (prev: {
       jq --raw-input --slurp '(split("\n") | map(select(. != "")))'"$*"
     }
     ln -s $onboardingArtifacts onboarding
-    tomlq -i '.extra.onsite = true' config.toml
-    tomlq -i ".extra.nixos_version = $(cat onboarding/version | slurp_jq '[0]')"
-    tomlq -i ".extra.isos = $(
-      find -L onboarding/systems -name '*.iso' | slurp_jq
+    tomlq -ti '.extra.onsite = true' config.toml
+    tomlq -ti ".extra.nixos_version = $({ cat onboarding/version || true; } | slurp_jq '[0]')" config.toml
+    tomlq -ti ".extra.isos = $(
+      { find -L onboarding/systems -name '*.iso' || true; } | slurp_jq
     )" config.toml
-    tomlq -i ".extra.vmas = $(
-      find -L onboarding/systems -name '*.vma.zst' | slurp_jq
+    tomlq -ti ".extra.vmas = $(
+      { find -L onboarding/systems -name '*.vma.zst' || true; } | slurp_jq
     )" config.toml
-    tomlq -i ".extra.manual = $(
-      find -L onboarding/manual -name index.html | slurp_jq '[0]'
+    tomlq -ti ".extra.manual = $(
+      { find -L onboarding/manual -name index.html || true; } | slurp_jq '[0] // ""'
     )" config.toml
-    tomlq -i ".extra.search = $(
-      find -L onboarding/search -name index.html | slurp_jq '[0]'
+    tomlq -ti ".extra.search = $(
+      { find -L onboarding/search -name index.html || true; } | slurp_jq '[0] // ""'
     )" config.toml
-    tomlq -i ".extra.nixpkgs = $(
-      find -L onboarding -name *.tar.gz | slurp_jq '[0]'
+    tomlq -ti ".extra.nixpkgs = $(
+      { find -L onboarding/nixpkgs -name *.tar.gz || true; } | slurp_jq '[0] // ""'
     )" config.toml
   '';
 
   postInstall = ''
-    ln -s $onboardingArtifacts $out/onboarding
+    ln -s $onboardingArtifacts $out/public/onboarding
   '';
 })
