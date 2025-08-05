8fcb465b7780574a4969ec8cf062d1e31aedb801
Morgan Jones <me@numin.it>
2025-08-05 14:22:05 -0700

    onboarding: more updates, including channels

--- pkgs/nix-vegas-site/onsite.nix
+++ pkgs/nix-vegas-site/onsite.nix
@@ -14,27 +14,33 @@ nix-vegas-site.overrideAttrs (prev: {
     slurp_jq() {
       jq --raw-input --slurp '(split("\n") | map(select(. != "")))'"$*"
     }
-    ln -s $onboardingArtifacts onboarding
+    ln -s $onboardingArtifacts nixos
+    tomlq -ti '.base_url = "https://nixos.lv/"' config.toml
     tomlq -ti '.extra.onsite = true' config.toml
-    tomlq -ti ".extra.nixos_version = $({ cat onboarding/version || true; } | slurp_jq '[0]')" config.toml
+    tomlq -ti ".extra.nixpkgs_rev = $({ cat nixos/rev || true; } | slurp_jq '[0] // ""')" config.toml
+    tomlq -ti ".extra.nixos_version = $({ cat nixos/version || true; } | slurp_jq '[0] // ""')" config.toml
     tomlq -ti ".extra.isos = $(
-      { find -L onboarding/systems -name '*.iso' || true; } | slurp_jq
+      { find -L nixos/systems/ -name '*.iso' || true; } | slurp_jq
     )" config.toml
     tomlq -ti ".extra.vmas = $(
-      { find -L onboarding/systems -name '*.vma.zst' || true; } | slurp_jq
+      { find -L nixos/systems/ -name '*.vma.zst' || true; } | slurp_jq
     )" config.toml
     tomlq -ti ".extra.manual = $(
-      { find -L onboarding/manual -name index.html || true; } | slurp_jq '[0] // ""'
+      { find -L nixos/manual/ -name index.html || true; } | slurp_jq '[0] // ""'
     )" config.toml
     tomlq -ti ".extra.search = $(
-      { find -L onboarding/search -name index.html || true; } | slurp_jq '[0] // ""'
+      { find -L nixos/search/ -name index.html || true; } | slurp_jq '[0] // ""'
     )" config.toml
-    tomlq -ti ".extra.nixpkgs = $(
-      { find -L onboarding/nixpkgs -name *.tar.gz || true; } | slurp_jq '[0] // ""'
+    tomlq -ti ".extra.channel = $(
+      { find -L nixos/channel/ -name '*.tar.xz' || true; } | slurp_jq '[0] // ""'
     )" config.toml
+    rm -f nixos
+
+    echo "Final config.toml:" >&2
+    cat config.toml >&2
   '';
 
   postInstall = ''
-    ln -s $onboardingArtifacts $out/public/onboarding
+    ln -s $onboardingArtifacts $out/public/nixos
   '';
 })
