145cdac4c66653b9f85b78e2cbac7cd1d03dfbca
Morgan Jones <me@numin.it>
2026-04-01 23:08:37 -0700

    flake, netlify: update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix Vegas Website";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
   };
